83fbea5843fb1578d054506a62ed61e51f313f3f
Neil Campbell <neildcampbell@hotmail.com>
2019-10-23 09:14:16 +0100

    refactorred initial if statements

--- spec/schoolReportSpec.js
+++ spec/schoolReportSpec.js
@@ -4,16 +4,21 @@ describe('SCHOOL REPORT', function(){
     it('should return REd:1 when Red is the string', function(){
         string = "Red"
         result = checkResult(string)
-        expect(result).toEqual('Red: 1')
+        expect(result).toEqual('Red: 1 Green: 0 Amber: 0')
     })
     it('should return Green:1 when Green is the string', function(){
         string = "Green"
         result = checkResult(string)
-        expect(result).toEqual('Green: 1')
+        expect(result).toEqual('Red: 0 Green: 1 Amber: 0')
     })
     it('should return Amber:1 when Amber is the string', function(){
         string = "Amber"
         result = checkResult(string)
-        expect(result).toEqual('Amber: 1')
+        expect(result).toEqual('Red: 0 Green: 0 Amber: 1')
+    })
+    it('should return Red 2 and the others 0', function(){
+        string = 'Red,Red'
+        result = checkResult(string)
+        expect(result).toEqual('Red: 2 Green: 0 Amber: 0')
     })
 })
--- src/schoolReport.js
+++ src/schoolReport.js
@@ -1,10 +1,19 @@
 function checkResult(string){
-    if (string == 'Red'){
-        return 'Red: 1'
-    } else if (string === 'Green'){
-        return 'Green: 1'
-    } else if (string === 'Amber'){
-        return 'Amber: 1'
-    }
+    let newArray = string.split(",")
+    let countAmber = 0
+    let countGreen = 0
+    let countRed = 0
+    newArray.forEach(function(item){
+        if (item === 'Red'){
+            countRed++
+        } else if (item === 'Green'){
+            countGreen ++
+        } else if (item === 'Amber'){
+           countAmber++
+        }
+
+    })
+    return `Red: ${countRed} Green: ${countGreen} Amber: ${countAmber}`
+    
     
 }
